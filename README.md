4da7f52d53b8f71211400a562f317f27b72bd670
Yoshimura Yuu <m@yyu.pw>
2016-07-09 16:00:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+The Rust Programming Language Ja PDF
+================================
+
+[![Build Status](https://travis-ci.org/y-yu/trpl-ja-pdf.svg?branch=master)](https://travis-ci.org/y-yu/trpl-ja-pdf)
+
+このリポジトリは[the-rust-programming-language-ja](https://github.com/rust-lang-ja/the-rust-programming-language-ja)の
+PDFファイルをTravis CIによって生成するものである。
+
+## 成果物
+
+- [a4.pdf](https://y-yu.github.io/trpl-ja-pdf/a4.pdf)
+- [letter.pdf](https://y-yu.github.io/trpl-ja-pdf/letter.pdf)
---
+++ options.rs
@@ -0,0 +1,9 @@
+pub const RELEASE_DATE: &'static str = "2016-06-02";
+
+pub const MARKDOWN: &'static str = "markdown+grid_tables+pipe_tables-simple_tables+raw_html+implicit_figures+footnotes+intraword_underscores+auto_identifiers-inline_code_attributes";
+
+pub const HTML: &'static str = "--smart --normalize --standalone --self-contained --highlight-style=tango --table-of-contents --section-divs --template=lib/template.html --css=lib/pandoc.css --to=html5";
+
+pub const EPUB: &'static str = "--smart --normalize --standalone --self-contained --highlight-style=tango --epub-stylesheet=lib/epub.css --table-of-contents";
+
+pub const LATEX: &'static str = "--smart --normalize --standalone --self-contained --highlight-style=tango --chapters --table-of-contents --template=lib/template.tex --latex-engine=lualatex --to=latex";
